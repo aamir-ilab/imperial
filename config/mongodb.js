46ae2c474a10e197e13ea344e34e2c80900b3284
Webmaster <admin@britmanagement.co.uk>
2020-10-05 21:28:56 +0100

    v2

--- config/mongodb.js
+++ config/mongodb.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 module.exports = async() => {
     // return mongoose.connect('mongodb+srv://xxxxxxxxx/xxxx', { useNewUrlParser: true, useUnifiedTopology: true })
-    return mongoose.connect('mongodb+srv://pixify:BHtw6eyV9buRlkxK@@imperialrecruitment.afyiy.mongodb.net', { useNewUrlParser: true, useUnifiedTopology: true })
+    return mongoose.connect('mongodb+srv://pixify:BHtw6eyV9buRlkxK@imperialrecruitment.afyiy.mongodb.net', { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => console.log("MongoDB successfully connected"))
         .catch(err => console.log(err));
 };
